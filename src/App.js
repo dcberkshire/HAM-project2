4222bdc23837c156e247f3ee20c810779141e15f
dberkshire <dberkshireee@gmail.com>
2022-01-05 11:23:23 -0800

    attempt to scaffold api setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+const searchOptions = {
+  api: 'https://api.harvardartmuseums.org',
+  key: process.env.REACT_API_KEY
+}
   return (
     <div className="App">
       <header className="App-header">
+          
       </header>
     </div>
   );
