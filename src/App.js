22320d4e555948bf0f52bac55bb9f51a39d1c9d2
dberkshire <dberkshireee@gmail.com>
2022-01-06 14:56:32 -0800

    add art api to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
+	const [artCollections, setArtCollections] = useState({});
+	const getArt = (search = '') => {
+		if (search) {
+			search = `&q=${search}`;
+		}
+
+		let url = `https://api.harvardartmuseums.org/image?apikey=${process.env.REACT_APP_API_KEY}${search}`;
+
+		fetch(url)
+			.then((response) => {
+				return response.json();
+			})
+			.then((res) => {
+				setArtCollections(res);
+			})
+			.catch((error) => console.log('error', error));
+	};
+
+	useEffect(() => {
+		getArt();
+	}, []);
+
+	console.log(artCollections, 'art');
 	return (
 		<>
 			<header className='App-header'>
