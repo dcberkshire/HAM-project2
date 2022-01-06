3456e113c61cd5474ec72c9f105e7ee4e633088a
dberkshire <dberkshireee@gmail.com>
2022-01-06 11:02:39 -0800

    layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
 function App() {
-
-const searchOptions = {
-  api: 'https://api.harvardartmuseums.org',
-  key: process.env.REACT_API_KEY
-}
-  return (
-    <div className="App">
-      <header className="App-header">
-          
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<header className='App-header'>
+				<h1></h1>
+			</header>
+			<main></main>
+		</>
+	);
 }
 
 export default App;
